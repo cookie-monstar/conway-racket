7a7441e0027aed5a557c03a4b0860429a8c3677e
Nikhil Reddy Ramolla <rnr1410@gmail.com>
2018-04-06 03:03:23 +0530

    Databasing Simulations

---
+++ simulations.rkt
@@ -0,0 +1,63 @@
+#lang racket
+(provide (all-defined-out))
+(define dictionary (list
+(list "Gosper glider gun"
+"24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4b
+obo$10bo5bo7bo$11bo3bo$12b2o" 36 9)
+
+
+(list "pulsar" 
+"2b3o3b3o2b2$o4bobo4bo$o4bobo4bo$o4bobo4bo$2b3o3b3o2b2$2b3o3b3o2b$o4bob
+o4bo$o4bobo4bo$o4bobo4bo2$2b3o3b3o" 13 13) ;(period 3)
+
+
+(list "Beacon" 
+"2o2b$o3b$3bo$2b2o" 4 4);(period 2)
+
+
+(list "Pentadecathlon"
+"2bo4bo2b$2ob4ob2o$2bo4bo" 10 3) ;(period 15)
+
+
+(list "Toad"
+"b3o$3" 4 2);(period 2)
+
+
+(list "Blinker"
+"3o" 3 1);(period 2)
+
+
+(list "Glider"
+"bob$2bo$3o" 3 3)
+
+
+(list "Light weight spaceship" 
+"bo2bo$o4b$o3bo$4o" 5 4) ;(period 4)
+
+
+(list "Block"
+"2o$2o" 2 2)
+
+(list "Beehive"
+"b2ob$o2bo$b2o" 4 3)
+
+(list "Loaf"
+"b2ob$o2bo$bobo$2bo" 4 4)
+
+
+(list "Replicator"
+"2b3o$bo2bo$o3bo$o2bob$3o" 5 5)
+
+
+(list "Aircraft carrier"
+"2o2b$o2bo$2b2o"  4 3)))
+
+
+
+
+
+
+
+
+
+
