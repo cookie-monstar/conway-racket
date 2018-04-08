2fa466facb9c48fb68062b2b06006e48e7393fb0
Nikhil Reddy Ramolla <rnr1410@gmail.com>
2018-04-08 13:29:08 +0530

    removing unnecessary functions

--- gui.rkt
+++ gui.rkt
@@ -9,10 +9,3 @@
   (foldr (lambda (a b) (overlay/xy a x y b)) (rectangle 0 0 100 "red") lst))
 (define (grid-draw grid)
   (list-draw 0 8 (map (lambda (r) (list-draw 8 0 (map (lambda (c) (if (= c 1) live dead)) r))) grid)))
-
-(define (animator grid)
-  (lambda
-    (n)
-    (begin
-      (set! grid (grid-next grid))
-      (grid-draw grid))))
