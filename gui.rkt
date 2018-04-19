e1a3a8616e306818ccf64879aebb394ec17cc3f2
Nikhil Reddy Ramolla <rnr1410@gmail.com>
2018-04-20 04:42:10 +0530

    Wow

--- gui.rkt
+++ gui.rkt
@@ -21,36 +21,42 @@
        [min-width 256]
        [min-height 768]))
 (define play-state #f)
+(define patch-state #f)
 (define play-button
   (new button%
        [parent panel-right]
        [label "Paused"]
-       [callback (lambda (button event) (set! play-state (not play-state)) (send play-button set-label (if play-state "Playing" "Paused")))]))
-(define grid-select
-  (new 
-(define timer #f)
-(define canvas
-  (new canvas%
-       [parent panel-left]
-       ;[style '(gl)]
-       ;[border 1]
-       [min-width 768]
-       [min-height 768]
-       [paint-callback (lambda (canvas dc)
-                         (set! timer
-                           (new timer%
-                                [notify-callback (lambda () (draw-canvas canvas dc))]
-                                [interval 100])))]))
+       [callback (lambda (button event) (set! play-state (not play-state))
+                   (send play-button set-label (if play-state "Playing" "Paused")))]))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define live (make-object brush% "BLACK" 'solid))
 (define dead (make-object brush% "WHITE" 'solid))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define (draw-canvas canvas dc)
-  (map (lambda (row)
-         (map (lambda (col)
-                (send dc set-brush (if (= 1 (list-ref (list-ref grid row) col)) live dead))
-                (send dc draw-rectangle (* 12 col) (* 12 row) 12 12))
-              (range (length (car grid)))))
-       (range (length grid)))
-	   (if play-state (set! grid (grid-next grid)) (void)))
+(define (draw-canvas canvas dc play-state)
+  (if play-state
+      (begin
+        (displayln "Wow")
+        (map (lambda (row)
+               (map (lambda (col)
+                      (send dc set-brush (if (= 1 (list-ref (list-ref grid row) col)) live dead))
+                      (send dc draw-rectangle (* 12 col) (* 12 row) 12 12))
+                    (range (length (car grid)))))
+             (range (length grid)))
+        (set! grid (grid-next grid)))
+      (void)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define grid (grid-expand (rle->grid (car (dict-ref hangar "gosper glider gun"))) 64 64))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define timer #f)
+(define canvas
+  (new canvas%
+       [parent panel-left]
+       [min-width 768]
+       [min-height 768]
+       [paint-callback (lambda (canvas dc)
+                         (draw-canvas canvas dc #t)
+                         (set! timer
+                               (new timer%
+                                    [notify-callback (lambda () (draw-canvas canvas dc play-state))]
+                                    [interval 100]))
+                         )]))
